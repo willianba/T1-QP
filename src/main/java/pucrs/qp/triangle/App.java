a4d3ced9601d764c943c133889aba5fee4e02b25
Willian Alves <wba.alves@gmail.com>
2020-04-14 03:16:43 -0300

    add a lot of new unit tests

--- src/main/java/pucrs/qp/triangle/App.java
+++ src/main/java/pucrs/qp/triangle/App.java
@@ -1,23 +1,33 @@
 package pucrs.qp.triangle;
 
+import javax.management.InvalidAttributeValueException;
+
 /**
  * @author marco.mangan@pucrs.br
  */
 public class App {
-  public static int identificaTriangulo(int a, int b, int c) {
-    if ((a < b + c) && (b < a + c) && (c < b + a)) {
-      if ((a == b) && (b == c))
-        return Tipos.EQUILATERO.value();
-      else if ((a != b) && (a != c) && (b != c))
-        return Tipos.ESCALENO.value();
-      else
-        return Tipos.ISOSCELES.value();
+  public static int identificaTriangulo(int a, int b, int c) throws InvalidAttributeValueException {
+    if (a <= 0 || b <= 0 || c <= 0) {
+      throw new InvalidAttributeValueException("Todos os valores devem ser inteiros e maior de zero.");
+    } else {
+      if ((a < b + c) && (b < a + c) && (c < b + a)) {
+        if ((a == b) && (b == c))
+          return Tipos.EQUILATERO.value();
+        else if ((a != b) && (a != c) && (b != c))
+          return Tipos.ESCALENO.value();
+        else
+          return Tipos.ISOSCELES.value();
+      }
+      return Tipos.INVALIDO.value();
     }
-    return Tipos.INVALIDO.value();
   }
 
   public static void main(String[] args) {
-    System.out.println("Triângulos!");
-    System.out.println(identificaTriangulo(2, 3, 4));
+    try {
+      System.out.println("Triângulos!");
+      System.out.println(identificaTriangulo(2, 3, 4));
+    } catch (InvalidAttributeValueException ex) {
+      System.out.println(ex.getMessage());
+    }
   }
 }
--- src/test/java/pucrs/qp/triangle/AppTest.java
+++ src/test/java/pucrs/qp/triangle/AppTest.java
@@ -2,34 +2,98 @@ package pucrs.qp.triangle;
 
 import static org.junit.Assert.assertEquals;
 
+import javax.management.InvalidAttributeValueException;
+
 import org.junit.Test;
 
 public class AppTest {
   @Test
-  public void testEscaleno() {
+  public void testEscaleno() throws InvalidAttributeValueException {
     int actual = App.identificaTriangulo(2, 3, 4);
     int expected = Tipos.ESCALENO.value();
     assertEquals(expected, actual);
   }
 
   @Test
-  public void testEquilatero() {
+  public void testEquilatero() throws InvalidAttributeValueException {
     int actual = App.identificaTriangulo(5, 5, 5);
     int expected = Tipos.EQUILATERO.value();
     assertEquals(expected, actual);
   }
 
   @Test
-  public void testIsosceles() {
+  public void testIsosceles() throws InvalidAttributeValueException {
     int actual = App.identificaTriangulo(6, 6, 7);
     int expected = Tipos.ISOSCELES.value();
     assertEquals(expected, actual);
   }
 
   @Test
-  public void testInvalido() {
-    int actual = App.identificaTriangulo(6, 6, -7);
+  public void primeiroVarianteDeTres() throws InvalidAttributeValueException {
+    int actual = App.identificaTriangulo(6, 7, 6);
+    int expected = Tipos.ISOSCELES.value();
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void segundoVarianteDeTres() throws InvalidAttributeValueException {
+    int actual = App.identificaTriangulo(7, 6, 6);
+    int expected = Tipos.ISOSCELES.value();
+    assertEquals(expected, actual);
+  }
+
+  @Test(expected = InvalidAttributeValueException.class)
+  public void valorZero() throws InvalidAttributeValueException {
+    App.identificaTriangulo(8, 0, 9);
+  }
+
+  @Test(expected = InvalidAttributeValueException.class)
+  public void valorNegativo() throws InvalidAttributeValueException {
+    App.identificaTriangulo(2, 3, -4);
+  }
+
+  @Test
+  public void valoresNaoTriangulares() throws InvalidAttributeValueException {
+    int actual = App.identificaTriangulo(1, 2, 3);
     int expected = Tipos.INVALIDO.value();
     assertEquals(expected, actual);
+  }
+
+  @Test
+  public void primeiroVarianteDeSete() throws InvalidAttributeValueException {
+    int actual = App.identificaTriangulo(1, 3, 2);
+    int expected = Tipos.INVALIDO.value();
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void segundoVarianteDeSete() throws InvalidAttributeValueException {
+    int actual = App.identificaTriangulo(3, 1, 2);
+    int expected = Tipos.INVALIDO.value();
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void linhaLonga() throws InvalidAttributeValueException {
+    int actual = App.identificaTriangulo(1, 2, 8);
+    int expected = Tipos.INVALIDO.value();
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void varianteDeNove() throws InvalidAttributeValueException {
+    int actual = App.identificaTriangulo(8, 1, 2);
+    int expected = Tipos.INVALIDO.value();
+    assertEquals(expected, actual);
+  }
+
+  @Test(expected = InvalidAttributeValueException.class)
+  public void todosZero() throws InvalidAttributeValueException {
+    App.identificaTriangulo(0, 0, 0);
+  }
+
+  @Test(expected = InvalidAttributeValueException.class)
+  public void testInvalido() throws InvalidAttributeValueException {
+    App.identificaTriangulo(6, 6, -7);
   }
 }
